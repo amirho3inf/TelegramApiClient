8492315d4fa3aca22756167762f586edb41e69c4
amirho3inf <mramirho3inf@gmail.com>
2018-08-15 19:55:47 +0430

    Update smth

---
+++ Examples/echo.py
@@ -0,0 +1,11 @@
+from TelegramApiClient import Client, Filters
+
+Api_Token = "123456789:AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+client = Client(Api_Token)
+bot = client.Bot()
+
+@client.message(Filters.text)
+def MessageHandler(message, data):
+    message.reply(message.text)
+
+client.run()
--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
 import setuptools
 
-long_description = open("README.md", "r").read()
 install_requires = ['telepot==12.6']
 setuptools.setup(
-    name="TelegramApiBot",
-    version="1",
+    name="TelegramApiClient",
+    version="0.1",
     author="AmirHo3inF",
     author_email="MrAmirho3inf@gmail.com",
     description="Telepot upgraded version",
     install_requires=install_requires,
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description='',
     url="https://github.com/amirho3inf/TelegramApiClient",
     license='MIT',
     packages=setuptools.find_packages(),
